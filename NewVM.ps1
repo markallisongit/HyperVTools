02f988e29e8cf51d1de45d1d0304f718a4cbd5ab
Mark Allison <Mark.Allison@sabin.io>
2018-05-31 16:12:08 +0100

    securestring fun

--- NewVM.ps1
+++ NewVM.ps1
@@ -4,11 +4,11 @@ param
     [Parameter(Position = 0, Mandatory = $true)]
     [string]$ConfigFilePath,
     [Parameter(Position = 1, Mandatory = $true)]
-    [securestring]$AdministratorPassword,
+    [string]$AdministratorPassword,
     [Parameter(Position = 2, Mandatory = $false)]
-    [securestring]$DomainJoinPassword
+    [string]$DomainJoinPassword
 )
 
 # wrapper for automation
 Import-Module .\HyperVTools.psd1 -Force
-New-HyperVVM -ConfigFilePath $ConfigFilePath -AdministratorPassword $AdministratorPassword -DomainJoinPassword $DomainJoinPassword -Verbose
+New-HyperVVM -ConfigFilePath $ConfigFilePath -AdministratorPassword ($AdministratorPassword | ConvertTo-SecureString -AsPlainText -Force)  -DomainJoinPassword ($DomainJoinPassword | ConvertTo-SecureString -AsPlainText -Force)  -Verbose
